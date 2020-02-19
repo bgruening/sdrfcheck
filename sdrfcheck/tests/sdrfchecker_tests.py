00c65992151e3597a6c16b18e015c1b20d4fa2c2
Yasset Perez-Riverol <ypriverol@gmail.com>
2020-02-19 12:00:19 +0000

    schema updates

---
+++ sdrfcheck/tests/sdrfchecker_tests.py
@@ -0,0 +1,17 @@
+from click.testing import CliRunner
+from sdrfcheck.sdrfchecker import cli
+
+
+def validate_srdf():
+    """
+    Test the default behaviour of the vcf-to-proteindb tool
+    :return:
+    """
+    runner = CliRunner()
+    result = runner.invoke(cli,
+                           ['validate-sdrf', '--sdrf_file', 'testdata/sdrf.t    xt'])
+    assert result.exit_code == 0
+
+
+if __name__ == '__main__':
+    validate_srdf()
