86a74b38ee9e1f28d631546b6a2de4afcf865f3d
Björn Grüning <bjoern@gruenings.eu>
2020-03-01 19:37:05 +0100

    try adding entry points

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ def readme():
     with open('README.md') as f:
         return f.read()
 
+ENTRY_POINTS = '''
+        [console_scripts]
+        sdrtcheck=sdrfcheck.sdrfcheck:cli
+'''
 
 setup(name='sdrfcheck',
       version='0.0.4',
@@ -13,6 +17,7 @@ setup(name='sdrfcheck',
       long_description=readme(),
       long_description_content_type='text/markdown',
       author='BigBio Team',
+      entry_points=ENTRY_POINTS,
       author_email='ypriverol@gmail.com',
       license='LICENSE',
       include_package_data=True,
