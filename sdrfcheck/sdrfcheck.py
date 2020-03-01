2cde4f6d61b1246459c7a7686aa570361f2da60c
Björn Grüning <bjoern@gruenings.eu>
2020-03-01 19:41:06 +0100

    add some text in case of valid validation

--- sdrfcheck/sdrfcheck.py
+++ sdrfcheck/sdrfcheck.py
@@ -35,6 +35,9 @@ def validate_sdrf(ctx, sdrf_file: str, template: str, check_ms):
     if check_ms:
         df.validate(MASS_SPECTROMETRY)
 
+    # provide some info to the user, as no info is confusing
+    print('Everything seems to fine. Well done.')
+
 
 cli.add_command(validate_sdrf)
 
